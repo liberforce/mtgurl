b0add11bd3fe1770552eb6c26d63a9890a365291
Luis Menina <liberforce@freeside.fr>
2017-10-13 13:25:54 +0200

    Factorize into a function, make it self-executable

--- mtgurl.py
+++ mtgurl.py
@@ -1,7 +1,13 @@
+#! /usr/bin/env python3
 import requests
 
-r = requests.get('http://gatherer.wizards.com/Pages/Search/Default.aspx?name=%2b%5bSynod%20Sanctum%5d')
-print(r.status_code)
+def fetch_card(url):
+    r = requests.get(url)
+    print(r.status_code)
 
-with open('toto.html', 'w') as file_:
-    file_.write(r.text)
+    with open('toto.html', 'w') as file_:
+        file_.write(r.text)
+
+if __name__ == '__main__':
+    url = 'http://gatherer.wizards.com/Pages/Search/Default.aspx?name=%2b%5bSynod%20Sanctum%5d'
+    fetch_card(url)
