c0d8e1dc4be1ebd97d561ab4101c63db183388c5
Luis Menina <liberforce@freeside.fr>
2018-11-05 12:13:35 +0100

    Run both quoted and unquoted tests

--- mtg-urlcheck.py
+++ mtg-urlcheck.py
@@ -4,17 +4,17 @@ import unittest
 import os.path
 
 
-def make_url1(name):
+def make_unquoted_url(name):
     base_url='http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+[%s]'
     url = base_url % name
     return url
 
-def make_url2(name):
+def make_quoted_url(name):
     base_url='http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+["%s"]'
     url = base_url % name
     return url
 
-def fetch_card(name, make_url=make_url1):
+def fetch_card(name, make_url):
     url = make_url(name)
     r = requests.head(url, allow_redirects=False)
     r.connection.close()
@@ -22,7 +22,7 @@ def fetch_card(name, make_url=make_url1):
     return bool(r.status_code == 302)
 
 
-class TestFetchCard(unittest.TestCase):
+class TestFetchQuotedCard(unittest.TestCase):
     CARDS = []
 
     @classmethod
@@ -36,7 +36,23 @@ class TestFetchCard(unittest.TestCase):
         for card in self.CARDS:
             with self.subTest(i=card):
                 print('{}'.format(card))
-                self.assertTrue(fetch_card(card))
+                self.assertTrue(fetch_card(card, make_quoted_url))
+
+class TestFetchUnquotedCard(unittest.TestCase):
+    CARDS = []
+
+    @classmethod
+    def setUpClass(cls):
+        cards_db_path = os.path.join(os.path.dirname(__file__), 'cards.txt')
+        with open(cards_db_path, 'r') as file_:
+            for line in file_:
+                cls.CARDS.append(line.strip())
+
+    def test_fetch_card(self):
+        for card in self.CARDS:
+            with self.subTest(i=card):
+                print('{}'.format(card))
+                self.assertTrue(fetch_card(card, make_unquoted_url))
 
 
 if __name__ == '__main__':
