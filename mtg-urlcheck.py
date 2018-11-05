71da5fdaf5286760a46f877fe3a5781a436e1676
Luis Menina <liberforce@freeside.fr>
2018-11-05 17:47:27 +0100

    Add vanilla URLs for testing

--- mtg-urlcheck.py
+++ mtg-urlcheck.py
@@ -12,6 +12,9 @@ def make_unquoted_url(name):
 def make_quoted_url(name):
     return base_url + ('+["%s"] =' % name)
 
+def make_vanilla_url(name):
+    return base_url + name
+
 def fetch_card(name, make_url):
     url = make_url(name)
     r = requests.head(url, allow_redirects=False)
@@ -51,6 +54,22 @@ class TestFetchUnquotedCard(unittest.TestCase):
             with self.subTest(i=card):
                 print('{}'.format(card))
                 self.assertTrue(fetch_card(card, make_unquoted_url))
+
+class TestFetchVanillaCard(unittest.TestCase):
+    CARDS = []
+
+    @classmethod
+    def setUpClass(cls):
+        cards_db_path = os.path.join(os.path.dirname(__file__), 'cards.txt')
+        with open(cards_db_path, 'r') as file_:
+            for line in file_:
+                cls.CARDS.append(line.strip())
+
+    def test_fetch_card(self):
+        for card in self.CARDS:
+            with self.subTest(i=card):
+                print('{}'.format(card))
+                self.assertTrue(fetch_card(card, make_vanilla_url))
 
 
 if __name__ == '__main__':
