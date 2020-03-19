34573076c658d58efddfed064b50a4a581029fd1
Luis Menina <liberforce@freeside.fr>
2020-03-19 01:22:13 +0100

    Fix private global variable naming convention

--- mtgurl/__init__.py
+++ mtgurl/__init__.py
@@ -2,16 +2,16 @@
 
 import requests
 
-base_url = 'https://gatherer.wizards.com/Pages/Card/Details.aspx?name='
+_BASE_URL = 'https://gatherer.wizards.com/Pages/Card/Details.aspx?name='
 
 def make_unquoted_url(name):
-    return base_url + ('+[%s]' % name)
+    return _BASE_URL + ('+[%s]' % name)
 
 def make_quoted_url(name):
-    return base_url + ('+["%s"]' % name)
+    return _BASE_URL + ('+["%s"]' % name)
 
 def make_vanilla_url(name):
-    return base_url + name
+    return _BASE_URL + name
 
 def fetch_by_name(name, url_method):
     url = url_method(name)
