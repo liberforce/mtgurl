a2a5c1c9144d873f3d3bdc4394eba3fcfe5134be
Luis Menina <liberforce@freeside.fr>
2020-03-25 10:45:02 +0100

    Add tests using different asynchronous implementations
    Blocking for response, getting responses on-the fly with callbacks, etc.

--- tests/async/test_fetch_card_vanilla.py
+++ tests/async/test_fetch_card_vanilla.py
@@ -2,6 +2,8 @@
 import os.path
 from pprint import pprint as pp
 
+import grequests
+import requests
 import pytest
 
 import mtgurl
@@ -26,8 +28,61 @@ CARDS = list_card_names(
 )
 
 
-def test_fetch_card_vanilla():
+def test_fetch_cards_sending_requests_all_at_once_blocking_for_responses():
+    """
+    Send requests blocking for the response.
+    Block until all responses are received.
+    """
     urls = [mtgurl.make_vanilla_url(cardname) for cardname in CARDS]
     pp(urls)
     statuses = mtgurl.fetch_by_url_async(urls)
     assert all(statuses.values())
+
+
+def test_fetch_cards_sending_requests_by_batches_blocking_for_responses():
+    """
+    Send requests blocking for the response.
+    Use a request pool to keep a threshold of maximum number of requests.
+    Block until all responses are received.
+    """
+    import time
+    start = time.time()
+    urls = [mtgurl.make_vanilla_url(cardname) for cardname in CARDS]
+    reqs = (grequests.head(url, allow_redirects=True) for url in urls)
+    responses = grequests.imap(reqs, size=30)
+
+    delay = 1
+    time.sleep(delay)
+
+    index = 0
+    for response in responses:
+        print(response.url)
+        assert response
+        assert response.status_code in [200]
+        index += 1
+
+    stop = time.time()
+    print(stop - start - delay)
+
+
+def _on_response(response, *args, **kwargs):
+    print(response.url)
+    assert response
+    assert response.status_code in [200]
+
+
+def test_fetch_cards_sending_requests_by_batches_not_blocking_for_responses():
+    """
+    Send requests but don't block for the response.
+    Use a request pool to keep a threshold of maximum number of requests.
+    Use a callback to get notified of the response.
+    """
+    urls = [mtgurl.make_vanilla_url(cardname) for cardname in CARDS]
+    reqs = (grequests.head(url, allow_redirects=True, callback=_on_response) for url in urls)
+    pool = grequests.Pool(30)
+    for req in reqs:
+        grequests.send(req, pool)
+
+    # Don't exit until we received the responses, otherwise we may lose some of them
+    import time
+    time.sleep(20)
